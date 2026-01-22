770e2217a85e33822d5283754d98ed0012b0acd6
Murali <imkrishna1311@gmail.com>
2026-01-23 01:35:34 +0530

    Changed DTO to use primitives and restrict adding null values into responses

--- src/main/java/com/kieru/backend/dto/ErrorResponse.java
+++ src/main/java/com/kieru/backend/dto/ErrorResponse.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Builder
 public class ErrorResponse {
     private LocalDateTime timestamp;
-    private int status;       // e.g. 400, 404, 500
+    private Integer status;       // e.g. 400, 404, 500
     private String error;     // e.g. "Bad Request"
     private String message;   // e.g. "Daily limit exceeded"
     private String path;      // e.g. "/api/secrets"
