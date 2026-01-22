a0c3f6594ac5d1580743035792ab479377ca1951
Murali <imkrishna1311@gmail.com>
2026-01-23 01:35:09 +0530

    modified table signature

--- src/main/java/com/kieru/backend/repository/UserRepository.java
+++ src/main/java/com/kieru/backend/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.kieru.backend.repository;
 
 import com.kieru.backend.entity.User;
+import com.kieru.backend.util.KieruUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface  UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 
-    String getSubscriptionById(String OwnerId);
+    @Query("SELECT u.subscription FROM User u WHERE u.id = :id")
+    KieruUtil.SubscriptionPlan findSubscriptionPlanById(@Param("id") String id);
 
 }
-
